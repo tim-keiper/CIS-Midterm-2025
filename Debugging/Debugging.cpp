0f65747f671376ae22b6d30469908b0584e601bf
TimKeiper <164468171+AurinSkyfire@users.noreply.github.com>
2025-04-06 12:46:27 -0700

    Commit Debug code before debugging

---
+++ Debugging/Debugging.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int number = 10;
+    if (number = 20) {
+        cout << "Number is 20" << endl;
+    }
+    else {
+        cout << "Number is not 20" << endl;
+    }
+
+    for (int i = 0; i <= 5; i++);
+    cout << i << endl;
+
+    return 0;
+}
